37cebf42dc79f7a3d8f7091843c616cc5f32c4c4
venivateva <venivateva@gmail.com>
2019-09-10 15:15:24 -0400

    tooltips

--- SoccerStats.py
+++ SoccerStats.py
@@ -10,24 +10,24 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 dashapp = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-#### data exploration ######
+# data exploration ######
 
-datafile=pan.read_csv("/Users/venelinavateva/PycharmProjects/Soccer/soccerstats.csv",
-                    index_col=0)
+# datafile=pan.read_csv("/Users/venelinavateva/PycharmProjects/Soccer/soccerstats.csv", index_col=0)
+datafile=pan.read_csv("C:\PythonProjects\SoccerStats\soccerstats.csv", index_col=0)
 
 
 
-dframe=pan.DataFrame(datafile)
+dframe = pan.DataFrame(datafile)
 print(dframe)
 
-dcolumns=dframe.columns
-#print(dcolumns)
+dcolumns = dframe.columns
+# print(dcolumns)
 
-#types=dframe["Name","Value","Age","Nationality","Wage","Body Type","Joined","Work Rate"].dtypes
-#print(types)
+# types=dframe["Name","Value","Age","Nationality","Wage","Body Type","Joined","Work Rate"].dtypes
+# print(types)
 
 
-#Data Cleanup#
+# Data Cleanup#
 
 
 dframe[["Wage","Value"]]=dframe[["Wage","Value"]].replace(to_replace=r'[€]+',value='',regex=True)
@@ -39,54 +39,53 @@ dframe.Wage = (dframe.Wage.replace(r'[KM]+$', '', regex=True).astype(float) * \
 dframe.Wage.str.extract(r'[\d\.]+([KM]+)', expand=False).fillna(1).replace(['K','M'], [10**3, 10**6]).astype(int))
 
 
-#print(dframe[["Wage","Value"]].sort_values(by="Wage",ascending=True))
+# print(dframe[["Wage","Value"]].sort_values(by="Wage",ascending=True))
 
-
-
-sortedframe=dframe.sort_values(by="ID",ascending=False)
+sortedframe = dframe.sort_values(by="ID",ascending=False)
 
 sortedframe.drop_duplicates()
 sortedframe.dropna()
 
-smalldataset=sortedframe[0:1500]
+smalldataset = sortedframe[0:100]
 print(smalldataset[["Name","Value","Age","Nationality","Wage","Body Type","Joined","Work Rate"]])
 
-#
-# dashapp.layout = html.Div([
-#     dcc.Graph(
-#         id='Nationality and Wage',
-#         figure={
-#             'data': [
-#                 go.Scatter(
-#                     x=sortedframe[sortedframe['Nationality'] == i]['Wage'],
-#                     y=sortedframe[sortedframe['Nationality'] == i]['Value'],
-#                     text=sortedframe[sortedframe['Nationality'] == i]['Nationality'],
-#                     mode='markers',
-#                     opacity=0.7,
-#                     marker={
-#                         'size': 15,
-#                         'line': {'width': 0.5, 'color': 'white'}
-#                     },
-#                     name=i
-#                 ) for i in sortedframe.Nationality.unique()
-#             ],
-#             'layout': go.Layout(
-#                 xaxis={'type': 'log', 'title': 'Wage'},
-#                 yaxis={'title': 'Value'},
-#                 margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-#                 legend={'x': 0, 'y': 1},
-#                 hovermode='closest'
-#             )
-#         }
-#     )
-# ])
-#
-# if __name__ == '__main__':
-#     dashapp.run_server(debug=True)
+
+dashapp.layout = html.Div([
+    dcc.Graph(
+        id='Nationality and Wage',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=sortedframe[sortedframe['Nationality'] == i]['Wage'],
+                    y=sortedframe[sortedframe['Nationality'] == i]['Value'],
+                    text=sortedframe[sortedframe['Nationality'] == i]['Nationality'],
+                    mode='markers',
+                    opacity=0.7,
+                    hoverinfo=sortedframe[sortedframe["Name"]==i]["Name"],
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in sortedframe.Nationality.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'Wage'},
+                yaxis={'title': 'Value'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
+
+if __name__ == '__main__':
+    dashapp.run_server(debug=True)
 
 
 
 
-#print(sortedframe)
+# print(sortedframe)
 
 
